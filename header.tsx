50c84bc156c613bdd114045cdbf2d7324e07d350
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2022-09-06 18:36:43 +0200

    demo remote component

---
+++ header.tsx
@@ -0,0 +1,11 @@
+import { h } from "preact";
+
+
+export default function MyHeader() {
+    return (
+      <div class={tw`p-4 mx-auto max-w-screen-md`}>
+       <H1>
+        This is my header :) 
+       </H1>
+      </div>
+    );
