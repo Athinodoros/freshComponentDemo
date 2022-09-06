b0316be449cf78c8754e7e1253543fcecabd9dd7
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2022-09-06 20:45:48 +0200

    test

--- RandomName.tsx
+++ RandomName.tsx
@@ -4,6 +4,8 @@
 
 const getRandomNumber = (min:number, max:number)=>{
     console.log(min,max);
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+    
     let res = Math.floor(Math.random() * (max - min + 1)) + min;
     return res;
     //Math.random() * (max - min) + min;
