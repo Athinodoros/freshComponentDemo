d34ca8f2253e7ac5932f5da5ecd35b786bdf0022
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2022-09-06 20:31:41 +0200

    correct

--- RandomName.tsx
+++ RandomName.tsx
@@ -3,7 +3,8 @@
 
 
 const getRandomNumber = (min:number, max:number)=>{
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    //Math.random() * (max - min) + min;
     //return Math.floor(Math.random() * max) + min;
 }
 
