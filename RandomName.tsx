cf6846bb04f4f9384d21aa95d5dd13ce77e3df7f
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2022-09-06 20:35:03 +0200

    logs

--- RandomName.tsx
+++ RandomName.tsx
@@ -3,7 +3,9 @@
 
 
 const getRandomNumber = (min:number, max:number)=>{
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(min,max);
+    let res = Math.floor(Math.random() * (max - min + 1)) + min;
+    return res;
     //Math.random() * (max - min) + min;
     //return Math.floor(Math.random() * max) + min;
 }
